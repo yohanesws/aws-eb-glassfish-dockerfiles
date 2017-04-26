63ab4c1756f91655afe2233d14540cda40171c62
Yohanes Widi Sono <yohanesws@Yohaness-MacBook-Pro.local>
2017-04-26 12:51:35 +0700

    template builder

--- 4.1-jdk8-aws-eb-onbuild/glassfish-start.sh
+++ 4.1-jdk8-aws-eb-onbuild/glassfish-start.sh
@@ -5,12 +5,22 @@ PID_FILE=$GLASSFISH_HOME/glassfish/domains/domain1/config/pid
 # when deploying a directory, Glassfish expect all submodules to be extracted
 # which is usually not the case for EARs
 # zip app back into a bundle and let Glassfish handle it
-rm -f /var/app/Dockerfile
-rm -f /var/app/Dockerrun.aws.json
-zip /var/app.zip -r .
+
+if ! [ -z $GLASSFISH_LIB_FOLDER ];then
+  cp $GLASSFISH_LIB_FOLDER/* $GLASSFISH_HOME/glassfish/domains/domain1/lib
+fi
+
+if ! [ -z $GLASSFISH_CONFIG ];then
+  cp $GLASSFISH_CONFIG/* $GLASSFISH_HOME/glassfish/domains/domain1/config
+fi
 
 asadmin start-domain
-asadmin deploy --contextroot / --name current-app /var/app.zip
+# asadmin deploy --contextroot / --name current-app /var/app.zip
+#read db config value
+
+
+if ! [ -z $GLASSFISH_APP_FOLDER ];then
+  cp $GLASSFISH_APP_FOLDER/* $GLASSFISH_HOME/glassfish/domains/domain1/autodeploy
+fi
 
 inotifywait -qq -e delete_self $PID_FILE
-
